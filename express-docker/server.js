5cdff44c3e7667d733c567ccd2b7a34806e05f73
kashivirus <kashivirus691@gmail.com>
2023-01-29 18:11:48 +0500

    first commit

---
+++ express-docker/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const PORT = process.env.PORT || 5500;
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+app.use(express.json());
+app.use(cors());
+
+// Database connection
+
+const url = "mongodb+srv://kashif:1234@nodeexpressprojects.wvdpn.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(url);
+const Product = mongoose.model('Product', { name: String, price: Number });
+app.get('/', (req, res) => {
+    return res.send('Welcome to Node js, express js in Docker');
+});
+
+app.post('/api/products', async (req, res) => {
+    const product = new Product({ name: req.body.name, price: req.body.price });
+    const savedProduct = await product.save();
+    return res.status(201).json(savedProduct);
+});
+
+app.get('/api/products', async (req, res) => {
+    const products = await Product.find();
+    return res.json(products);
+});
+
+app.delete('/api/products/:id', async (req, res) => {
+    const product = await Product.deleteOne({ id: req.params.id });
+    return res.json(product);
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
